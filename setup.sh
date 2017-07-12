b2f55593e1c0907ba174ca12befa6d913aa22bec
Pierre-Olivier Konecki <pkonecki@gmail.com>
2017-07-12 09:14:52 +0000

    remove chsh - ohmyzsh setup does it

--- setup.sh
+++ setup.sh
@@ -4,5 +4,4 @@ sudo apt install python-pip git zsh
 sudo pip2 install --upgrade pip
 sudo pip2 install dotfilemanager
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-chsh `which zsh`
 
