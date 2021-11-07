3a4d0af495d90a195c7cb2e3cf92a83722cb4eb8
root <root@hetz.kek.rip>
2021-11-07 20:13:06 +0100

    python3 upgrade

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd
-sudo apt install python-pip git zsh
-sudo pip2 install --upgrade pip
-sudo pip2 install dotfilemanager
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo apt install python3-pip git zsh
+sudo pip install --upgrade pip
+sudo pip install dotfilemanager
+#sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
